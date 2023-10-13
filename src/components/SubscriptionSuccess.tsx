7e33e0d589f39013d2f575e219baf5fa8dd4cf8f
Florin Atănăsoaei <atanasoaeiflorin@gmail.com>
2023-10-13 10:40:07 +0300

    Style SubscriptionSuccess on desktop devices

--- src/components/SubscriptionSuccess.tsx
+++ src/components/SubscriptionSuccess.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Container, Button } from '@mui/material';
+import { Box, Typography, Paper, Button } from '@mui/material';
 import SuccessIcon from './SuccessIcon';
 
 interface SubscriptionSuccessProps {
@@ -6,43 +6,73 @@ interface SubscriptionSuccessProps {
 }
 
 const SubscriptionSuccess = ({ onDismiss }: SubscriptionSuccessProps) => (
-  <Container>
-    <Box
-      component='section'
-      pt={14}
-      pb={4}
-      sx={{ height: '100vh' }}
-      display='flex'
-      flexDirection='column'
+  <Box
+    sx={(theme) => ({
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bgcolor: 'secondary.main',
+        height: '100%',
+        paddingTop: theme.spacing(20),
+        paddingBottom: theme.spacing(20),
+      },
+    })}
+  >
+    <Paper
+      sx={(theme) => ({
+        [theme.breakpoints.up('md')]: {
+          display: 'flex',
+          padding: theme.spacing(4),
+          borderRadius: theme.spacing(5),
+          maxWidth: '400px',
+        },
+      })}
     >
-      <Box flex={1}>
-        <Box pb={4}>
-          <SuccessIcon sx={{ fontSize: '64px' }} />
-        </Box>
-        <Typography variant='h1' pb={2} maxWidth='60%'>
-          Thanks for subscribing!
-        </Typography>
-        <Typography component='p'>
-          A confirmation email has been sent to{' '}
-          <Typography fontWeight={700} component='span'>
-            ash@loremcompany.com
-          </Typography>
-          . Please open it and click the button inside to confirm your
-          subscription.
-        </Typography>
-      </Box>
-      <Button
-        variant='contained'
-        color='secondary'
-        size='large'
-        fullWidth
-        sx={{ alignSelf: 'flex-end' }}
-        onClick={onDismiss}
+      <Box
+        component='section'
+        pt={{ xs: 14, md: 4 }}
+        pb={4}
+        px={4}
+        sx={(theme) => ({
+          height: '100vh',
+          [theme.breakpoints.up('md')]: {
+            height: 'auto',
+            paddingTop: theme.spacing(4),
+          },
+        })}
+        display='flex'
+        flexDirection='column'
       >
-        Dismiss message
-      </Button>
-    </Box>
-  </Container>
+        <Box flex={1} mb={{ md: 4 }}>
+          <Box pb={4}>
+            <SuccessIcon sx={{ fontSize: '64px' }} />
+          </Box>
+          <Typography variant='h1' pb={2} maxWidth={{ xs: '60%', md: '100%' }}>
+            Thanks for subscribing!
+          </Typography>
+          <Typography component='p'>
+            A confirmation email has been sent to{' '}
+            <Typography fontWeight={700} component='span'>
+              ash@loremcompany.com
+            </Typography>
+            . Please open it and click the button inside to confirm your
+            subscription.
+          </Typography>
+        </Box>
+        <Button
+          variant='contained'
+          color='secondary'
+          size='large'
+          fullWidth
+          sx={{ alignSelf: 'flex-end' }}
+          onClick={onDismiss}
+        >
+          Dismiss message
+        </Button>
+      </Box>
+    </Paper>
+  </Box>
 );
 
 export default SubscriptionSuccess;
